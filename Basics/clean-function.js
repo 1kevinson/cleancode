beb0fe3774627bd5a54e486a0273852b4a3b3d0e
Arsene Kevin <kevinkouomeu@icloud.com>
2020-11-23 07:03:27 +0100

    functions

---
+++ Basics/clean-function.js
@@ -0,0 +1,15 @@
+class User {
+
+    constructor(email, password) {
+        this.email = email;
+        this.password = password
+    }
+
+    save() {
+        db.insert('users',this)
+    }
+
+}
+
+const user = new User("kk@gm.fr","regex101");
+user.save();
